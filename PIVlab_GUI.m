70bc70eb54ce37e8ecc6f1add4e8c86bb10d93a2
trash <trash@WilliamSwitch5.fritz.box>
2021-02-02 23:06:41 +0100

    Simplified background subtraction UI seems to work, further testing needed.

--- PIVlab_GUI.m
+++ PIVlab_GUI.m
@@ -1945,12 +1945,15 @@ if get(handles.bg_subtract,'Value')==1 % Hier wird ja nur display gemacht. also 
 	if isempty(bg_img) %checkbox is enabled, but no bg is present
 		set(handles.bg_subtract,'Value',0);
 	else
-		currentimage = currentimage-bg_img;
+		if size(currentimage,3)>1 %color image cannot be displayed properly when bg subtraction is enabled.
+			currentimage = rgb2gray(currentimage)-bg_img;
+		else
+			currentimage = currentimage-bg_img;
+		end
 	end
 end
 
 function generate_BG_img
-
 handles=gethand;
 if get(handles.bg_subtract,'Value')==1
 	bg_img_A = retr('bg_img_A');
@@ -1958,17 +1961,132 @@ if get(handles.bg_subtract,'Value')==1
 	if isempty(bg_img_A) || isempty(bg_img_B)
 		answer = questdlg('Mean intensity background image needs to be calculated. Press ok to start.', 'Background subtraction', 'OK','Cancel','OK');
 		if strcmp(answer , 'OK')
-			disp('BG not present, calculating now')
-			%Calculate BG for all images....
-			bg_img_A = 0;
-			bg_img_B = 0;
-			put('bg_img_A',bg_img_A);
-			put('bg_img_B',bg_img_B);
+			%disp('BG not present, calculating now')
+			%% Calculate BG for all images....
+			% read first image to determine properties
+			filepath = retr('filepath');
+			if retr('video_selection_done') == 0
+				sequencer=retr('sequencer'); %Timeresolved or pairwise 0=timeres.; 1=pairwise
+				[~,~,ext] = fileparts(filepath{1});
+				if strcmp(ext,'.b16')
+					image1=f_readB16(filepath{1});
+					image2=f_readB16(filepath{2});
+					imagesource='b16_image';
+				else
+					image1=imread(filepath{1});
+					image2=imread(filepath{2});
+					imagesource='normal_pixel_image';
+				end
+			else
+				video_reader_object = retr('video_reader_object');
+				video_frame_selection=retr('video_frame_selection');
+				image1 = read(video_reader_object,video_frame_selection(1));
+				image2 = read(video_reader_object,video_frame_selection(2));
+				imagesource='from_video';
+				sequencer=0; %set sequencer to timeresolved for videos
+			end
+			if size(image1,3)>1
+				image1=rgb2gray(image1);
+				image2=rgb2gray(image2);
+				colorimg=1;
+			else
+				colorimg=0;
+			end
+			counter=1;
+			classimage=class(image1); %memorize the original image format
+			
+			if strcmp(classimage,'double')==1
+				image1=int64(image1*1024); %convert to int64 to accept very large numbers
+				image2=int64(image2*1024);
+			else
+				image1=int64(image1); %convert to int64 to accept very large numbers
+				image2=int64(image2);
+			end
+			if sequencer==0 %time-resolved
+				start_bg=2;
+				skip_bg=1;
+			else
+				start_bg=3;
+				skip_bg=2;
+			end
+			%perform image addition
+			%if timeresolved: generate only one background image from all
+			%images
+			%if not: generate two background images. One from even frames,
+			%one from odd frames
+			toolsavailable(0)
+			for i=start_bg:skip_bg:size(filepath,1)
+				counter=counter+1;
+				set(handles.preview_preprocess, 'String', ['Progress: ' num2str(round(i/size(filepath,1)*99)) ' %']);drawnow;
+				if strcmp('b16_image',imagesource)
+					image_to_add1 = f_readB16(filepath{i});
+					if sequencer==1 %not time-resolved
+						image_to_add2 = f_readB16(filepath{i+1});
+					end
+				elseif strcmp('normal_pixel_image',imagesource)
+					image_to_add1 = imread(filepath{i});
+					if sequencer==1 %not time-resolved
+						image_to_add2 = imread(filepath{i+1});
+					end
+				elseif strcmp('from_video',imagesource)
+					image_to_add1 = read(video_reader_object,video_frame_selection(i));
+					if sequencer==1 %not time-resolved
+						image_to_add2 = read(video_reader_object,video_frame_selection(i+1));
+					end
+				end
+				if strcmp(classimage,'double')==1
+					image_to_add1=image_to_add1*1024;
+					if sequencer==1 %not time-resolved
+						image_to_add2=image_to_add2*1024;
+					end
+				end
+				if colorimg==1
+					image_to_add1 = rgb2gray(image_to_add1);
+					if sequencer==1 %not time-resolved
+						image_to_add2 = rgb2gray(image_to_add2);
+					end
+				end
+				
+				image1=image1+int64(image_to_add1);
+				if sequencer==1 %not time-resolved
+					image2=image2+int64(image_to_add2);
+				end
+			end
+			%Making average in the format of the original image
+			if strcmp(classimage,'uint16')==1
+				image1_bg=uint16(image1/counter);
+				if sequencer==1 %not time-resolved
+					image2_bg=uint16(image2/counter);
+				end
+			end
+			if strcmp(classimage,'uint8')==1
+				image1_bg=uint8(image1/counter);
+				if sequencer==1 %not time-resolved
+					image2_bg=uint8(image2/counter);
+				end
+			end
+			if strcmp(classimage,'double')==1
+				image1_bg=double(image1);
+				image1_bg=(image1_bg/counter/1024);
+				if sequencer==1 %not time-resolved
+					image2_bg=double(image2);
+					image2_bg=(image2_bg/counter/1024);
+				end
+			end
+			%make results accessible to the rest of the GUI:
+			put('bg_img_A',image1_bg);
+			if sequencer==1 %not time-resolved
+				put('bg_img_B',image2_bg);
+			else
+				put('bg_img_B',image1_bg); %timeresolved --> same bg image for a and b
+			end
+			set(handles.preview_preprocess, 'String', 'Preview current frame');drawnow;
+			toolsavailable(1)
 		else % user has checkbox enabled, but doesn't want to calculate the background...
 			set(handles.bg_subtract,'Value',0);
 		end
 	else
-		disp('BG exists')
+		%disp('BG exists')
 	end
 end
 
@@ -2584,6 +2702,8 @@ if getappdata(hgui,'video_selection_done')
 	put('yzoomlimit',[]);
 	sliderrange
 	set (handles.filenamebox, 'string', filename);
+	put('bg_img_A',[]);
+	put('bg_img_B',[]);
 	put ('resultslist', []); %clears old results
 	put ('derived',[]);
 	put('displaywhat',1);%vectors
