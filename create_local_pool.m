b4f81380510f3929aaafd0d055a01b1d95bcec1b
Quynh Nguyen <qmn203@nyu.edu>
2021-04-16 17:19:07 -0400

    refactor: create separate modules, making functions available for othe modules

---
+++ create_local_pool.m
@@ -0,0 +1,12 @@
+function create_local_pool(default_core, option_core)
+    % create a local parallel pool
+    % default_core: number of cores in the machine 
+    % option_core: number of cores specified by user
+
+    poolobj = gcp('nocreate'); % get current pool object
+    
+    if isempty(poolobj)  % if no pool has been created 
+        parpool('local',min(default_core,option_core))
+    end
+
+end
