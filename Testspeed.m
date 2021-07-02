689150312e9fa39598beb3e262440037c74352dc
trash <trash@Williamx13.fritz.box>
2021-07-02 11:04:31 +0200

    Added speed comparison between serial and parallel comouting

--- Testspeed.m
+++ Testspeed.m
@@ -2,15 +2,19 @@
 clear all
 close all
 clc
+try
+    delete(gcp('nocreate'))
+catch
+end
 disp('This script performs the four most time consuming calculations in PIVlab')
 disp('and measures the time to complete them.')
 disp('...testing calculation speed, please wait...')
 %%
 dccspeed=zeros(1,128);
-    disp('...testing DCC...')
+disp('...testing DCC...')
 for j=1:128
     if mod(j,1.28)==0
-    fprintf('%d%%\n',(round(j/128*100)));drawnow;
+        fprintf('%d%%\n',(round(j/128*100)));drawnow;
     end
     %A=rand(64,64)*255;
     %B=rand(128,128)*255;
@@ -25,14 +29,14 @@ for j=1:128
         C=conv2(B,Atemp,'valid');
     end
     dccspeed(j)=toc/i*1000;
- 
+    
 end
 %%
 dftspeed=zeros(1,128);
-    disp('...testing DFT...')
+disp('...testing DFT...')
 for j=1:128
     if mod(j,1.28)==0
-    fprintf('%d%%\n',(round(j/128*100)));drawnow;
+        fprintf('%d%%\n',(round(j/128*100)));drawnow;
     end
     
     A=round(rand(j,j)*255);
@@ -69,6 +73,42 @@ for i = 1:1600
     F=interp2(A,B,double(C),D,E,'*spline');
 end
 splspeed=toc/i*1000;
+
+%%
+disp('...checking parallel computing availability...')
+try
+    ppool=parpool;
+    if ppool.Connected == 1
+        parallel_avail=1;
+    else
+        parallel_avail=0;
+    end
+catch
+    parallel_avail=0;
+end
+if parallel_avail==1
+    disp('...testing parallel computing performance...')
+    tic
+    %parallel
+    parfor k=1:50
+        for j=1:10000
+            A=round(rand(64,64)*255);
+            B=round(rand(64,64)*255);
+            C =fftshift(real(ifft2(conj(fft2(A)).*fft2(B)))); %full line of code
+        end
+    end
+    parallel_speed=toc;
+    %serial
+    tic
+    for k=1:50
+        for j=1:1000 %10x less tests for serial
+            A=round(rand(64,64)*255);
+            B=round(rand(64,64)*255);
+            C =fftshift(real(ifft2(conj(fft2(A)).*fft2(B)))); %full line of code
+        end
+    end
+    serial_speed=toc*10;
+end
 disp('...finished')
 %%
 disp('----------------------------------------')
@@ -77,21 +117,11 @@ disp(['mean DCC calculation speed:   ' num2str(mean(dccspeed)) ' ms'])
 disp(['mean DFT calculation speed:   ' num2str(mean(dftspeed)) ' ms'])
 disp(['Linear interpolation speed:   ' num2str(linspeed) ' ms'])
 disp(['Spline interpolation speed:   ' num2str(splspeed) ' ms'])
+if parallel_avail==1
+    disp(['Speed increase with parallel computing:   ' num2str(round(serial_speed/parallel_speed,1)) 'x'])
+end
 ax=plotyy([1:128],dccspeed,[1:128],dftspeed);
 title('Calculation speed [ms] of DCC and DFT')
 xlabel('interrogation window size [px]')
 ylabel(ax(1), 'DCC speed [ms]');
 ylabel(ax(2), 'DFT speed [ms]');
-%% Williams results
-%{
-Acer Switch5
-Intel Core i5-7200U CPU @ 2.50GHz
-8 GB RAM
-64 bit Win 10 Home
-MATLAB 8.6.0.267246 (R2015b)
-
-mean DCC calculation speed:   10.2573 ms
-mean DFT calculation speed:   0.59817 ms
-Linear interpolation speed:   0.28329 ms
-Spline interpolation speed:   0.87221 ms
-%}
