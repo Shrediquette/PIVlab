d5be5ef2dd8d56e3c7f6de35556124b4257c05a9
Shrediquette <42935970+Shrediquette@users.noreply.github.com>
2022-07-09 11:03:16 +0200

    Added mean brightness indicator

--- PIVlab_capture_resources/PIVlab_capture_sharpness_indicator.m
+++ PIVlab_capture_resources/PIVlab_capture_sharpness_indicator.m
@@ -2,12 +2,15 @@ function [sharpness,sharpness_map]=PIVlab_capture_sharpness_indicator (input_ima
 skip=3;
 diff_image=abs(diff(input_image(1:skip:end,1:skip:end))); %process only every 3rd pixel.... still works?
 sharpness=std2(single(diff_image));
-sharpness=sharpness/mean(input_image(:))*10000;
+mean_brightess = mean(input_image(:));
+sharpness=sharpness/mean_brightess*10000;
+
 delete(findobj('tag','sharpness_display_text'));
 if ~isempty(textx) && ~isempty(texty)
 	hgui=getappdata(0,'hgui');
 	PIVlab_axis = findobj(hgui,'Type','Axes');
 	text(textx,texty,['Sharpness: ' int2str(sharpness)],'Color',[1 1 0],'tag','sharpness_display_text','Horizontalalignment','right','Parent',PIVlab_axis);
+	text(textx,texty*0.95,['Mean brightness: ' int2str(mean_brightess)],'Color',[1 1 0],'tag','sharpness_display_text','Horizontalalignment','right','Parent',PIVlab_axis);
 end
 
 sharpness_map=[];
