dd490c7179e03150ae8685ad625d26ed3aea2d3c
Shrediquette <42935970+Shrediquette@users.noreply.github.com>
2022-11-02 19:44:56 +0100

    Added parallel processing for background subtraction

--- PIVlab_GUI.m
+++ PIVlab_GUI.m
@@ -2630,6 +2630,205 @@ currentimage(currentimage<0)=0; %bg subtraction may yield negative
 %in the results however.
 
 
+function generate_BG_img_parallel
+handles=gethand;
+if get(handles.bg_subtract,'Value')==1
+	bg_img_A = retr('bg_img_A');
+	bg_img_B = retr('bg_img_B');
+	sequencer=retr('sequencer');%Timeresolved or pairwise 0=timeres.; 1=pairwise
+	if sequencer ~= 2 % bg subtraction only makes sense with time-resolved and pairwise sequencing style, not with reference style.
+		if isempty(bg_img_A) || isempty(bg_img_B)
+			answer = questdlg('Mean intensity background image needs to be calculated. Press ok to start.', 'Background subtraction', 'OK','Cancel','OK');
+			if strcmp(answer , 'OK')
+				%disp('BG not present, calculating now')
+				%% Calculate BG for all images....
+				% read first image to determine properties
+				filepath = retr('filepath');
+				if retr('video_selection_done') == 0
+					[~,~,ext] = fileparts(filepath{1});
+					if strcmp(ext,'.b16')
+						image1=f_readB16(filepath{1});
+						image2=f_readB16(filepath{2});
+						imagesource='b16_image';
+					else
+						image1=imread(filepath{1});
+						image2=imread(filepath{2});
+						imagesource='normal_pixel_image';
+					end
+				else
+					video_reader_object = retr('video_reader_object');
+					video_frame_selection=retr('video_frame_selection');
+					image1 = read(video_reader_object,video_frame_selection(1));
+					image2 = read(video_reader_object,video_frame_selection(2));
+					imagesource='from_video';
+				end
+				classimage=class(image1); %memorize the original image format (double, uint8 etc)
+
+				if size(image1,3)>1
+					image1=rgb2gray(image1); %rgb2gray conserves the variable class (single, double, uint8, uint16)
+					image2=rgb2gray(image2);
+					colorimg=1;
+				else
+					colorimg=0;
+				end
+				counter=1;
+
+				%convert all image types to double, ranging from 0...1
+				if strcmp(classimage,'double')==1 %double stays double
+					%do nothing
+				elseif strcmp(classimage,'single')==1 %e.g. 32bit tif, ranges from 0...1
+					image1=double(image1);
+					image2=double(image2);
+				elseif strcmp(classimage,'uint16')==1 %e.g. 16bit tif, ranges from 0...65535
+					image1=double(image1)/65535;
+					image2=double(image2)/65535;
+				elseif strcmp(classimage,'uint8')==1 %0...255
+					image1=double(image1)/255;
+					image2=double(image2)/255;
+				end
+				if sequencer==0 %time-resolved
+					start_bg=2;
+					skip_bg=1;
+				else
+					start_bg=3;
+					skip_bg=2;
+				end
+				%perform image addition
+				%if timeresolved: generate only one background image from all
+				%images
+				%if not: generate two background images. One from even frames,
+				%one from odd frames
+				toolsavailable(0)
+				updatecntr=0;
+
+				%das innere zu einer function machen und außen parfor...?
+				%als erstes: normaler for loop erstellt feste Liste mit Dateinamen.
+				%aus denen holt sich parfor loop die infos
+				%loop unten kann so bleiben wie er ist, lädt aber nicht bilder, sondern schreibt dateinamen in liste
+				cntr=1;
+				imagelist_A=cell(0);
+				imagelist_B=cell(0);
+				for i=start_bg:skip_bg:size(filepath,1)
+
+					imagelist_A{cntr}=filepath{i};
+					if sequencer==1 %not time-resolved
+						imagelist_B{cntr}=filepath{i+1};
+					end
+
+					cntr=cntr+1;
+
+				end
+				
+hbar = pivprogress(numel(imagelist_A),handles.preview_preprocess);
+				parfor	i=1:numel(imagelist_A)
+					
+					image_to_add1=[];
+					image_to_add2=[];
+					counter=counter+1; %counts the amount of images --> do that elsewhere
+					if strcmp('b16_image',imagesource)
+						image_to_add1 = f_readB16(imagelist_A{i}); %will be double
+						if sequencer==1 %not time-resolved
+							image_to_add2 = f_readB16(imagelist_B{i});
+						end
+					elseif strcmp('normal_pixel_image',imagesource)
+						image_to_add1 = imread(imagelist_A{i});
+						if sequencer==1 %not time-resolved
+							image_to_add2 = imread(imagelist_B{i}); %will be double or uint8
+						end
+					elseif strcmp('from_video',imagesource)
+						disp('parallel bg calculation wird mit videoframes nicht gehen....')
+					end
+					%% convert images to a grayscale double
+					%images arrive in their original format here
+					%convert everything to grayscale and double [0...1]
+					if colorimg==1
+						image_to_add1 = rgb2gray(image_to_add1); %will conserve image class
+						if sequencer==1 %not time-resolved
+							image_to_add2 = rgb2gray(image_to_add2);
+						end
+					end
+			
+					if strcmp(classimage,'single')==1
+						image_to_add1=double(image_to_add1);
+						if sequencer==1 %not time-resolved
+							image_to_add2=double(image_to_add2);
+						end
+					end
+					if strcmp(classimage,'uint8')==1
+						image_to_add1=double(image_to_add1)/255;
+						if sequencer==1 %not time-resolved
+							image_to_add2=double(image_to_add2)/255;
+						end
+					end
+					if strcmp(classimage,'uint16')==1
+						image_to_add1=double(image_to_add1)/65535;
+						if sequencer==1 %not time-resolved
+							image_to_add2=double(image_to_add2)/65535;
+						end
+					end
+
+					%now everything is double [0...1]
+
+					%% sum images
+					image1=image1 +image_to_add1;
+					if sequencer==1 %not time-resolved
+						image2=image2+image_to_add2;
+					end
+					hbar.iterate(1); %#ok<*PFBNS>
+				end %of for loop and image summing
+				close(hbar);
+
+				set (handles.preview_preprocess, 'string', 'Apply and preview current frame');
+				%divide the sum by the amount of summed images
+				image1_bg=image1/counter;
+				if sequencer==1 %not time-resolved
+					image2_bg=image2/counter;
+				end
+
+				%Convert back to original image class, if not double anyway
+				if strcmp(classimage,'uint8')==1 %#ok<*STISA>
+					image1_bg=uint8(image1_bg*255);
+					if sequencer==1 %not time-resolved
+						image2_bg=uint8(image2_bg*255);
+					end
+				end
+				if strcmp(classimage,'single')==1
+					image1_bg=single(image1_bg);
+					if sequencer==1 %not time-resolved
+						image2_bg=single(image2_bg);
+					end
+				end
+				if strcmp(classimage,'uint16')==1
+					image1_bg=uint16(image1_bg*65535);
+					if sequencer==1 %not time-resolved
+						image2_bg=uint16(image2_bg*65535);
+					end
+				end
+
+				%make results accessible to the rest of the GUI:
+				put('bg_img_A',image1_bg);
+				if sequencer==1 %not time-resolved
+					put('bg_img_B',image2_bg);
+				else
+					put('bg_img_B',image1_bg); %timeresolved --> same bg image for a and b
+				end
+				set(handles.preview_preprocess, 'String', 'Apply and preview current frame');drawnow;
+				toolsavailable(1)
+			else % user has checkbox enabled, but doesn't want to calculate the background...
+				set(handles.bg_subtract,'Value',0);
+			end
+		else
+			%disp('BG exists')
+		end
+
+	else
+		set(handles.bg_subtract,'Value',0);
+		warndlg(['Background removal is only available with the following sequencing styles:' sprintf('\n') '* Time resolved: [A+B], [B+C], [C+D], ...' sprintf('\n') '* Pairwise: [A+B], [C+D], [E+F], ...'])
+		uiwait
+	end
+end
+
+
 function generate_BG_img
 handles=gethand;
 if get(handles.bg_subtract,'Value')==1
@@ -2700,13 +2899,20 @@ if get(handles.bg_subtract,'Value')==1
 				%one from odd frames
 				toolsavailable(0)
 				updatecntr=0;
+
+				%das innere zu einer function machen und außen parfor...?
+				%als erstes: normaler for loop erstellt feste Liste mit Dateinamen.
+				%aus denen holt sich parfor loop die infos
+				%loop unten kann so bleiben wie er ist, lädt aber nicht bilder, sondern schreibt dateinamen in liste
 				for i=start_bg:skip_bg:size(filepath,1)
-					counter=counter+1;
-					updatecntr=updatecntr+1;
+					counter=counter+1; %counts the amount of images --> do that elsewhere
+					%% update progress bar
+					updatecntr=updatecntr+1; 
 					if updatecntr==5
 						set(handles.preview_preprocess, 'String', ['Progress: ' num2str(round(i/size(filepath,1)*99)) ' %']);drawnow expose;
 						updatecntr=0;
 					end
+					%% read image 1 and 2, different functions for different image types
 					if strcmp('b16_image',imagesource)
 						image_to_add1 = f_readB16(filepath{i}); %will be double
 						if sequencer==1 %not time-resolved
@@ -2723,6 +2929,8 @@ if get(handles.bg_subtract,'Value')==1
 							image_to_add2 = read(video_reader_object,video_frame_selection(i+1));
 						end
 					end
+					
+					%% convert images to a grayscale double
 					%images arrive in their original format here
 					%convert everything to grayscale and double [0...1]
 					if colorimg==1
@@ -2755,14 +2963,19 @@ if get(handles.bg_subtract,'Value')==1
 							image_to_add2=double(image_to_add2)/65535;
 						end
 					end
+
 					%now everything is double [0...1]
-					%Sum up  all images
+					
+					%% check if image size matches other images
+					% remove this, takes only time
 					img_size_info1=size(image1);
 					img_size_info2=size(image_to_add1);
 					if img_size_info1(1) ~= img_size_info2(1) || img_size_info1(2) ~= img_size_info2(2)
 						uiwait(warndlg('Error: All images in a session  MUST have the same size!'));
 						break
 					end
+
+					%% sum images
 					image1=image1 +image_to_add1;
 					if sequencer==1 %not time-resolved
 						img_size_info1=size(image2);
@@ -2774,6 +2987,7 @@ if get(handles.bg_subtract,'Value')==1
 						image2=image2+image_to_add2;
 					end
 				end %of for loop and image summing
+
 
 				%divide the sum by the amount of summed images
 				image1_bg=image1/counter;
@@ -4615,7 +4829,11 @@ if size(filepath,1) >1 || retr('video_selection_done') == 1
 	toggler=retr('toggler');
 	filepath=retr('filepath');
 	selected=2*floor(get(handles.fileselector, 'value'))-(1-toggler);
-	generate_BG_img
+	if retr('video_selection_done') == 0 % this is not nice, duplicated functions, one for parallel and one for video....
+		generate_BG_img_parallel
+	else
+		generate_BG_img
+	end
 	[img,~]=get_img(selected);
 	clahe=get(handles.clahe_enable,'value');
 	highp=get(handles.enable_highpass,'value');
@@ -11169,7 +11387,12 @@ if get(handles.bg_subtract,'Value')==1
 	sequencer=retr('sequencer');%Timeresolved or pairwise 0=timeres.; 1=pairwise
 	if sequencer ~= 2 % bg subtraction only makes sense with time-resolved and pairwise sequencing style, not with reference style.
 		if isempty(bg_img_A) || isempty(bg_img_B)
-			generate_BG_img
+			if retr('video_selection_done') == 0 % this is not nice, duplicated functions, one for parallel and one for video....
+				generate_BG_img_parallel
+			else
+				generate_BG_img
+			end
+
 			bg_img_A = retr('bg_img_A');
 			bg_img_B = retr('bg_img_B');
 		end
