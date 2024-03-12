50cbf3705288c3d8e3d17e029cbe995e0383d8d1
Mikhil Shetty <111747532+Mikhil11@users.noreply.github.com>
2024-03-12 14:29:15 +0530

    Delete +acquisition_NameSpace directory

--- +acquisition_NameSpace/acquisition_customWait.m
+++
@@ -1,9 +0,0 @@
-function pos = acquisition_customWait(hROI)
-% Listen for mouse clicks on the ROI
-l = addlistener(hROI,'ROIClicked',@roi_NameSpace.roi_ROIclickCallback);
-% Block program execution
-uiwait;
-% Remove listener
-delete(l);
-% Return the current position
-pos = hROI.Position;
--- +acquisition_NameSpace/acquisition_update_ac_status.m
+++
@@ -1,8 +0,0 @@
-function acquisition_update_ac_status(status)
-handles=gui_NameSpace.gui_gethand;
-contents=get(handles.ac_msgbox,'String');
-try
-	contents=[status;contents];
-catch
-end
-set(handles.ac_msgbox,'String',contents);
