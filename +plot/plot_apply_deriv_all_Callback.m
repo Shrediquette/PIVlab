40d1edd375b008be005054fcef5d2232d4f973be
Shrediquette <42935970+Shrediquette@users.noreply.github.com>
2024-03-16 12:58:52 +0100

    Removed remaining "toc"

--- +plot/plot_apply_deriv_all_Callback.m
+++ +plot/plot_apply_deriv_all_Callback.m
@@ -2,7 +2,6 @@ function plot_apply_deriv_all_Callback(~, ~, ~)
 handles=gui.gui_gethand;
 filepath=gui.gui_retr('filepath');
 gui.gui_toolsavailable(0,'Busy, please wait...')
-tic
 for i=1:floor(size(filepath,1)/2)+1
 	deriv=get(handles.derivchoice, 'value');
 	plot.plot_derivative_calc(i,deriv,1)
@@ -13,5 +12,3 @@ set (handles.apply_deriv_all, 'string', 'Apply to all frames');
 gui.gui_toolsavailable(1)
 gui.gui_update_progress(0)
 gui.gui_sliderdisp(gui.gui_retr('pivlab_axis'))
-toc
-
