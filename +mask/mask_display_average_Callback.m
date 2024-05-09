3b193ad4d736513a10252a8e50e4066bd95af5f7
Shrediquette <42935970+Shrediquette@users.noreply.github.com>
2024-05-09 09:43:50 +0200

    Pressing cancel when calculating background image resulted in error, fixed now.

--- +mask/mask_display_average_Callback.m
+++ +mask/mask_display_average_Callback.m
@@ -17,16 +17,17 @@ if isempty(bg_img_A) || isempty(bg_img_B)
 	bg_img_A = gui.gui_retr('bg_img_A');
 	bg_img_B = gui.gui_retr('bg_img_B');
 end
-pivlab_axis=gui.gui_retr('pivlab_axis');
-bg_img_AB=double((bg_img_A+bg_img_B)/2);
-bg_img_AB=bg_img_AB/max(bg_img_AB(:));
-bg_img_AB = repmat(bg_img_AB,1,1,3);
-image(bg_img_AB, 'parent',pivlab_axis, 'cdatamapping', 'scaled');
-colormap('gray');
-axis image;
-set(gca,'ytick',[])
-set(gca,'xtick',[])
-
+if ~isempty(bg_img_A)
+	pivlab_axis=gui.gui_retr('pivlab_axis');
+	bg_img_AB=double((bg_img_A+bg_img_B)/2);
+	bg_img_AB=bg_img_AB/max(bg_img_AB(:));
+	bg_img_AB = repmat(bg_img_AB,1,1,3);
+	image(bg_img_AB, 'parent',pivlab_axis, 'cdatamapping', 'scaled');
+	colormap('gray');
+	axis image;
+	set(gca,'ytick',[])
+	set(gca,'xtick',[])
+end
 if bg_was_on==0
 	set(handles.bg_subtract,'Value',0)
 	gui.gui_put('bg_img_A',[]);
