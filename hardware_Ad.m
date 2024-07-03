c268962d0099bfa4eb894b081cb9b0dfb4eee9c3
Shrediquette <42935970+Shrediquette@users.noreply.github.com>
2024-07-03 08:33:49 +0200

    Hardware add in image acquisition module: Better image. Larger Font.

--- hardware_Ad.m
+++ hardware_Ad.m
@@ -14,13 +14,13 @@ item=[0 0 0 0];
 
 
 item=[0 item(2)+item(4) parentitem(3) 2];
-handles.text0 = uicontrol(fig_handle,'Style','text','units', 'characters','Fontweight','bold', 'Fontunits','points','Fontsize',12,'Horizontalalignment', 'left','position',[item(1)+margin parentitem(4)-item(4)-margin-item(2) item(3)-margin*2 item(4)],'String','Did you know?');
+handles.text0 = uicontrol(fig_handle,'Style','text','units', 'characters','Fontweight','bold', 'Fontunits','points','Fontsize',14,'Horizontalalignment', 'left','position',[item(1)+margin parentitem(4)-item(4)-margin-item(2) item(3)-margin*2 item(4)],'String','Did you know?');
 
-item=[0 item(2)+item(4) parentitem(3) 1];
-handles.text05 = uicontrol(fig_handle,'Style','text','units', 'characters', 'Fontweight','bold','Fontunits','points','Horizontalalignment', 'left','position',[item(1)+margin parentitem(4)-item(4)-margin-item(2) item(3)-margin*2 item(4)],'String','PIVlab has it''s own set of LASERs, cameras and synchronizers!');
+item=[0 item(2)+item(4) parentitem(3) 2];
+handles.text05 = uicontrol(fig_handle,'Style','text','units', 'characters', 'Fontweight','bold','Fontunits','points','Fontsize',12,'Horizontalalignment', 'left','position',[item(1)+margin parentitem(4)-item(4)-margin-item(2) item(3)-margin*2 item(4)],'String','PIVlab has it''s own set of LASERs, cameras and synchronizers!');
 
 item=[0 item(2)+item(4) parentitem(3) 7];
-handles.text1 = uicontrol(fig_handle,'Style','text','units', 'characters','Fontunits','points','Horizontalalignment', 'left','position',[item(1)+margin parentitem(4)-item(4)-margin-item(2) item(3)-margin*2 item(4)],'String',{'This allows you to control and perform a complete PIV experiment directly in PIVlab. It makes flow research super easy and quick.' 'Learn more about William''s low-cost hardware on the website of his company:'});
+handles.text1 = uicontrol(fig_handle,'Style','text','units', 'characters','Fontunits','points','Fontsize',12,'Horizontalalignment', 'left','position',[item(1)+margin parentitem(4)-item(4)-margin-item(2) item(3)-margin*2 item(4)],'String',{'This allows you to control and perform a complete PIV experiment directly in PIVlab. It makes flow research super easy and quick.' '' 'Learn more about William''s low-cost hardware on the website of his company:'});
 handles.text1.String = textwrap(handles.text1,handles.text1.String);
 
 colf=get(gcf,'color');
@@ -39,10 +39,10 @@ bgim(:,:,2)=bgim(:,:,2)*colf_2;
 bgim(:,:,3)=bgim(:,:,3)*colf_3;
 
 item=[0 item(2)+item(4) parentitem(3) 3];
-handles.vidsi = uicontrol(fig_handle,'Style','pushbutton','String','Watch Youtube video','CData',bgim,'Units','characters','Fontweight','bold', 'Fontunits','points','Fontsize',12,'Position',[item(1)+margin parentitem(4)-item(4)-margin-item(2) item(3)-margin*2 item(4)],'TooltipString','Opens William''s video on YOUTUBE','Callback',@gotovideo);
+handles.vidsi = uicontrol(fig_handle,'Style','pushbutton','String','Click to watch Youtube video','CData',bgim,'Units','characters','Fontweight','bold', 'Fontunits','points','Fontsize',12,'Position',[item(1)+margin parentitem(4)-item(4)-margin-item(2) item(3)-margin*2 item(4)],'TooltipString','Opens William''s video on YOUTUBE','Callback',@gotovideo);
 
 item=[0 item(2)+item(4) parentitem(3) 3];
-handles.websi = uicontrol(fig_handle,'Style','pushbutton','String','Visit www.optolution.com','CData',bgim,'Units','characters','Fontweight','bold', 'Fontunits','points','Fontsize',12,'Position',[item(1)+margin parentitem(4)-item(4)-margin-item(2) item(3)-margin*2 item(4)],'TooltipString','Opens the website of OPTOLUTION.com','Callback',@gotowebsite);
+handles.websi = uicontrol(fig_handle,'Style','pushbutton','String','Click to visit optolution.com','CData',bgim,'Units','characters','Fontweight','bold', 'Fontunits','points','Fontsize',12,'Position',[item(1)+margin parentitem(4)-item(4)-margin-item(2) item(3)-margin*2 item(4)],'TooltipString','Opens the website of OPTOLUTION.com','Callback',@gotowebsite);
 
 item=[0 item(2)+item(4) parentitem(3) 1];
 handles.websi = uicontrol(fig_handle,'Style','checkbox','String','Never in my life show this hint again','Units','characters', 'Fontunits','points','Position',[item(1)+margin parentitem(4)-item(4)-margin-item(2) item(3)-margin*2 item(4)],'TooltipString','Disable ad forever and ever','Callback',@disableAd);
@@ -55,7 +55,6 @@ axis off
 axes (axes_handle)
 imshow(imread('hardware_Ad.jpg'));drawnow
 
-
 	function gotowebsite(~,~)
 		web('https://www.optolution.com/en/products/particle-image-velocimetry-piv/')
 		pause(1)
@@ -63,7 +62,7 @@ imshow(imread('hardware_Ad.jpg'));drawnow
 	end
 
 	function gotovideo(~,~)
-		web('https://www.youtube.com/watch?v=8B5M31NWlJc')
+		web('https://www.youtube.com/watch?v=8IQSODoYUr0')
 		pause(1)
 		close(fig_handle)
 	end
