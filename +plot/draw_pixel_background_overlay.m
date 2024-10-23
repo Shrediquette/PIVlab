2feb13888527bd0783a3e29d67399eabe4d779f0
Shrediquette <42935970+Shrediquette@users.noreply.github.com>
2024-10-23 11:12:16 +0200

    Cahnged unit description from e.g. "[m/s]" to " in m/s" (because the former is wrong afaik).

--- +plot/draw_pixel_background_overlay.m
+++ +plot/draw_pixel_background_overlay.m
@@ -151,13 +151,13 @@ if ~isempty(derived) && size(derived,2)>=(currentframe+1)/2 && displaywhat > 1  
 		name=get(handles.derivchoice,'string');
 		if strcmp(name,'N/A') %user hasn't visited the derived panel before
 			if (gui.retr('calu')==1 || gui.retr('calu')==-1) && gui.retr('calxy')==1
-				set(handles.derivchoice,'String',{'Vectors [px/frame]';'Vorticity [1/frame]';'Magnitude [px/frame]';'u component [px/frame]';'v component [px/frame]';'Divergence [1/frame]';'Vortex locator [1]';'Simple shear rate [1/frame]';'Simple strain rate [1/frame]';'Line integral convolution (LIC) [1]' ; 'Vector direction [degrees]'; 'Correlation coefficient [-]'});
-				set(handles.text35,'String','u [px/frame]:')
-				set(handles.text36,'String','v [px/frame]:')
+				set(handles.derivchoice,'String',{'Vectors in px/frame';'Vorticity in 1/frame';'Magnitude in px/frame';'u component in px/frame';'v component in px/frame';'Divergence in 1/frame';'Vortex locator';'Simple shear rate in 1/frame';'Simple strain rate in 1/frame';'Line integral convolution (LIC)' ; 'Vector direction in degrees'; 'Correlation coefficient'});
+				set(handles.text35,'String','u in px/frame:')
+				set(handles.text36,'String','v in px/frame:')
 			else
-				set(handles.derivchoice,'String',{'Vectors [m/s]';'Vorticity [1/s]';'Magnitude [m/s]';'u component [m/s]';'v component [m/s]';'Divergence [1/s]';'Vortex locator [1]';'Simple shear rate [1/s]';'Simple strain rate [1/s]';'Line integral convolution (LIC) [1]'; 'Vector direction [degrees]'; 'Correlation coefficient [-]'});
-				set(handles.text35,'String','u [m/s]:')
-				set(handles.text36,'String','v [m/s]:')
+				set(handles.derivchoice,'String',{'Vectors in m/s';'Vorticity in 1/s';'Magnitude in m/s';'u component in m/s';'v component in m/s';'Divergence in 1/s';'Vortex locator';'Simple shear rate in 1/s';'Simple strain rate in 1/s';'Line integral convolution (LIC)'; 'Vector direction in degrees'; 'Correlation coefficient'});
+				set(handles.text35,'String','u in m/s:')
+				set(handles.text36,'String','v in m/s:')
 			end
 			name=get(handles.derivchoice,'String');
 		end
