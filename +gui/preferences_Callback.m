b2ad1701fe92d4285f8d96f9a8a5c438bce8799e
Shrediquette <42935970+Shrediquette@users.noreply.github.com>
2025-02-09 21:30:59 +0100

    Fixed dark/light theme switching display

--- +gui/preferences_Callback.m
+++ +gui/preferences_Callback.m
@@ -4,4 +4,12 @@ handles=gui.gethand;
 panelwidth=gui.retr('panelwidth');
 set(handles.panelslider,'Value',panelwidth);
 gui.switchui('multip21')
+if ~verLessThan('Matlab','25')
+	current_theme = hgui.Theme.BaseColorStyle;
+	if strcmpi(current_theme, 'Dark')
+		set( handles.matlab_theme,'Value',1);
+	elseif strcmpi(current_theme, 'Light')
+		set( handles.matlab_theme,'Value',2);
+	end
+end
 
