53544cd1ab66a16e3d930e9bfc05cca49a5ce406
Shrediquette <42935970+Shrediquette@users.noreply.github.com>
2025-04-08 16:00:56 +0200

    Added option to select colorbar label number format and changed default to 'compact'.

--- +plot/draw_pixel_background_overlay.m
+++ +plot/draw_pixel_background_overlay.m
@@ -195,7 +195,13 @@ if ~isempty(derived) && size(derived,2)>=(currentframe+1)/2 && displaywhat > 1  
 		tickamount=min([colormap_steps 8])+1; % depends on the amount of colormap steps
 		coloobj.Ticks=linspace(0,colormap_steps,tickamount);
 		ticklabels=linspace(minscale_adjusted,maxscale_adjusted,tickamount);
-		ticklabels_string=num2str(ticklabels(:),'%0.3e');
+		if get(handles.colorbarnumberformat,'Value') == 1 % colorbar number format
+			ticklabels_string=num2str(ticklabels(:),'%0.3g');
+		elseif get(handles.colorbarnumberformat,'Value') == 2
+			ticklabels_string=num2str(ticklabels(:),'%0.3e');
+			elseif get(handles.colorbarnumberformat,'Value') == 3
+				ticklabels_string=num2str(ticklabels(:),'%0.3f');
+		end
 		coloobj.TickLabels =ticklabels_string;
 	end
 end
