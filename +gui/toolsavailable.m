46500ca10b617243f975f8321794c88e4818cd07
Shrediquette <42935970+Shrediquette@users.noreply.github.com>
2025-04-29 10:51:12 +0200

    Updated busy msg

--- +gui/toolsavailable.m
+++ +gui/toolsavailable.m
@@ -3,6 +3,7 @@ function toolsavailable(inpt,busy_msg)
 %1: re-enable tools that were previously also enabled
 hgui=getappdata(0,'hgui');
 handles=gui.gethand;
+pivlab_axis=gui.retr('pivlab_axis');
 if inpt==0
 	if get(handles.zoomon,'Value')==1
 		set(handles.zoomon,'Value',0);
@@ -21,7 +22,13 @@ end
 if exist('busy_msg','var') && ~isempty(busy_msg)
 	%additionally display banner that PIVlab is busy
 	if inpt==0
-		postix=get(gca,'XLim');postiy=get(gca,'YLim');text(postix(2)/2,postiy(2)/2,busy_msg,'HorizontalAlignment','center','VerticalAlignment','middle','color','y','fontsize',32, 'BackgroundColor', [0.25 0.25 0.25],'tag','busyhint','margin',30,'Clipping','on');
+		postix=get(pivlab_axis,'XLim');postiy=get(pivlab_axis,'YLim');
+		if verLessThan('matlab','25')
+			text(postix(2)/2,postiy(2)/2,busy_msg,'HorizontalAlignment','center','VerticalAlignment','middle','color','y','fontsize',32, 'BackgroundColor', [0.25 0.25 0.25],'tag','busyhint','margin',30,'Clipping','on');
+		else
+			rectangle('Position',[postix(2)/4*1,postiy(2)/4*1,postix(2)/2,postiy(2)/2],'Curvature',0.33,'FaceColor',[0.15 0.15 0.4],'FaceAlpha',0.5,'LineStyle','none','Tag','busyhint')
+			text(postix(2)/2,postiy(2)/2,busy_msg,'HorizontalAlignment','center','VerticalAlignment','middle','color','y','fontsize',32, 'BackgroundColor', 'none','tag','busyhint','margin',30,'Clipping','on');
+		end
 	end
 end
 elementsOfCrime=findobj(hgui, 'type', 'uicontrol');
