270e37d395aa77b44a5bc326d6fc048ae910d18b
Mat Tolladay <mat.tolladay@vortech.nl>
2025-08-21 16:31:13 +0200

    Added blob based feature detection

---
+++ Example_scripts/camera_correction.m
@@ -0,0 +1,16 @@
+function [outputArg1,outputArg2] = camera_correction(files, [10,7], "DICT_5X5_100", 100, 75)
+%CAMERA_CORRECTION Summary of this function goes here
+%   Detailed explanation goes here
+arguments (Input)
+    inputArg1
+    inputArg2
+end
+
+arguments (Output)
+    outputArg1
+    outputArg2
+end
+
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
