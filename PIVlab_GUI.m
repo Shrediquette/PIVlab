44f91fdb3766e581f029f32ff9263d59cf9b85c0
William Thielicke <w.th@gmx.de>
2025-09-23 13:27:44 +0200

    version no increase to make it visible on matlab fex

--- PIVlab_GUI.m
+++ PIVlab_GUI.m
@@ -44,7 +44,7 @@ if isempty(fh)
 	handles = guihandles; %alle handles mit tag laden und ansprechbar machen
 	guidata(MainWindow,handles)
 	setappdata(0,'hgui',MainWindow);
-	version = '3.11';
+	version = '3.12';
 	gui.put('PIVver', version);
 	try
 		warning off
