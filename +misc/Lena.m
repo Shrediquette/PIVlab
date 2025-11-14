fcacb33d0ac8b333652ffaf969fde21306f07903
William Thielicke <42935970+Shrediquette@users.noreply.github.com>
2025-11-14 12:32:23 +0100

    Add files via upload

--- +misc/Lena.m
+++ +misc/Lena.m
@@ -3,7 +3,7 @@ MainWindow=getappdata(0,'hgui');
 if strncmp (char(datetime('today')),'15-Oct',6)
 	yr=char(datetime('today'));
 	since=str2num(yr(8:11))-2005;
-	questdlg(['Loving Lena since ' num2str(since) ' years today!'],'It''s 15th of October!','Congratulations!','Congratulations!'); % #FallsNochRelevant
+	gui.custom_msgbox('quest',getappdata(0,'hgui'),'It''s 15th of October!',['Loving Lena since ' num2str(since) ' years today!'],'modal',{'Congratulations!'},'Congratulations!');
 	set(MainWindow, 'Name','Today it''s Lena-day!!')
 end
 
