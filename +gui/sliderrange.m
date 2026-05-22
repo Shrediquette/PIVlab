83118808d90e0f18a676176844181d60e40e4e26
William Thielicke <w.th@gmx.de>
2026-05-22 23:25:03 +0200

    In the image selection slider: Larger "box" to drag on.

--- +gui/sliderrange.m
+++ +gui/sliderrange.m
@@ -2,39 +2,39 @@ function sliderrange(reset)
 filepath=gui.retr('filepath');
 handles=gui.gethand;
 if gui.retr('video_selection_done') == 0
-	if size(filepath,1)>2
-		sliderstepcount=floor(size(filepath,1)/2);
-		set(handles.fileselector, 'enable', 'on');
-		slidersteps=[1/(sliderstepcount-1) 1/(sliderstepcount-1)*10];
-		if isinf(slidersteps(1))
-			slidersteps=[1 1];
-			set(handles.fileselector, 'enable', 'off');
-		end
-		if reset==1
-			set (handles.fileselector,'value',1, 'min', 1,'max',sliderstepcount,'sliderstep', slidersteps);
-		else
-			set (handles.fileselector, 'min', 1,'max',sliderstepcount,'sliderstep', slidersteps);
-		end
-	else
-		sliderstepcount=1;
-		set(handles.fileselector, 'enable', 'off');
-		if reset==1
-			set (handles.fileselector,'value',1, 'min', 1,'max',2,'sliderstep', [0.5 0.5]);
-		else
-			set (handles.fileselector, 'min', 1,'max',2,'sliderstep', [0.5 0.5]);
-		end
-	end
+    if size(filepath,1)>2
+        sliderstepcount=floor(size(filepath,1)/2);
+        set(handles.fileselector, 'enable', 'on');
+        amount_frames_per_large_step= ceil((sliderstepcount *0.1));
+        slidersteps=[1/(sliderstepcount-1) 1/(sliderstepcount-1)*amount_frames_per_large_step]; % sets steps to 1 frame (click on arrow) or 10% of whole range (click on trough)
+        if isinf(slidersteps(1))
+            slidersteps=[1 1];
+            set(handles.fileselector, 'enable', 'off');
+        end
+        if reset==1
+            set (handles.fileselector,'value',1, 'min', 1,'max',sliderstepcount,'sliderstep', slidersteps);
+        else
+            set (handles.fileselector, 'min', 1,'max',sliderstepcount,'sliderstep', slidersteps);
+        end
+    else
+        sliderstepcount=1;
+        set(handles.fileselector, 'enable', 'off');
+        if reset==1
+            set (handles.fileselector,'value',1, 'min', 1,'max',2,'sliderstep', [0.5 0.5]);
+        else
+            set (handles.fileselector, 'min', 1,'max',2,'sliderstep', [0.5 0.5]);
+        end
+    end
 else % a video has been imported
-	%video_frame_selection=retr('video_frame_selection');
-	%sliderstepcount=numel(video_frame_selection)/2;
-	%set(handles.fileselector, 'enable', 'on');
-	%set (handles.fileselector,'value',1, 'min', 1,'max',sliderstepcount,'sliderstep', [1/(sliderstepcount-1) 1/(sliderstepcount-1)*10]);
-	sliderstepcount=size(filepath,1)/2;
-	set(handles.fileselector, 'enable', 'on');
-	if reset==1
-		set (handles.fileselector,'value',1, 'min', 1,'max',sliderstepcount,'sliderstep', [1/(sliderstepcount-1) 1/(sliderstepcount-1)*10]);
-	else
-		set (handles.fileselector, 'min', 1,'max',sliderstepcount,'sliderstep', [1/(sliderstepcount-1) 1/(sliderstepcount-1)*10]);
-	end
+    %video_frame_selection=retr('video_frame_selection');
+    %sliderstepcount=numel(video_frame_selection)/2;
+    %set(handles.fileselector, 'enable', 'on');
+    %set (handles.fileselector,'value',1, 'min', 1,'max',sliderstepcount,'sliderstep', [1/(sliderstepcount-1) 1/(sliderstepcount-1)*10]);
+    sliderstepcount=size(filepath,1)/2;
+    set(handles.fileselector, 'enable', 'on');
+    if reset==1
+        set (handles.fileselector,'value',1, 'min', 1,'max',sliderstepcount,'sliderstep', [1/(sliderstepcount-1) 1/(sliderstepcount-1)*10]);
+    else
+        set (handles.fileselector, 'min', 1,'max',sliderstepcount,'sliderstep', [1/(sliderstepcount-1) 1/(sliderstepcount-1)*10]);
+    end
 end
-
